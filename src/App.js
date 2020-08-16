93f6bfaf405c7dc7924e8db9d442a464f7ad7b2a
Rohit Kumawat <rkumawat125@gmail.com>
2020-08-16 11:31:32 +0530

    intialization

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './components/Button';
+
+function App() {
+  return (
+      <div style={{display: "flex", alignContent: "center", justifyContent: "center", padding: "100px 0"}}>
+        <Button>Click me</Button>
+      </div>
+  );
+}
+
+export default App;
