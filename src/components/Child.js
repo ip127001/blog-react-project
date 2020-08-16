b5099d72590b41b29cff42bddb7663b8b9d21a9d
Rohit Kumawat <rkumawat125@gmail.com>
2020-08-16 17:53:22 +0530

    sandbox config added

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React from "react";
 
 class Child extends React.Component {
-    delay() {
-        console.log('[Delay] function called');
+  delay() {
+    console.log("[Delay] function called");
 
-        for(let i = 0; i < 5000000000; i++) {
-            i++;
-        }
-        return 'delayed text';
+    for (let i = 0; i < 800000000; i++) {
+      i++;
     }
+    return "delayed text";
+  }
 
-    render() {
-        console.log('[Child] rendered');
+  render() {
+    console.log("[Child] rendered");
 
-        return (
-            <div className="child">
-                <hr />
-                <div>{this.delay()}</div>
-                Selected Color: {this.props.color}
-            </div>
-        )
-    }
+    return (
+      <div className="child">
+        <hr />
+        <div>{this.delay()}</div>
+        Selected Color: {this.props.color}
+      </div>
+    );
+  }
 }
 
 export default Child;
